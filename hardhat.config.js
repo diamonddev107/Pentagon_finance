017fbdd26de7a682415d2d090b315997aa336453
diamonddev107 <kisney.dev.11@gmail.com>
2022-03-11 04:16:49 -0500

    add testnet configuration

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,28 @@ module.exports = {
   networks: {
     hardhat: {
 
-    }
+    },
+    ropsten: {
+      chainId: 3,
+      url: process.env.API_URL,
+      accounts: {
+        mnemonic: process.env.MNEMONIC
+      },
+      gas: 2100000, 
+      gasPrice: 8000000000
+    },
+    kovan: {
+      accounts: 
+      {
+          mnemonic: process.env.MNEMONIC,
+      },
+      url: "https://kovan.infura.io/v3/99b8947af7e14278ae235bb21eb81f53",
+      chainId: 42,
+      timeout: 200000,
+      gas: 2100000, 
+      gasPrice: 8000000000,
+      nonce:150
+    },
   },
   solidity: {
     compilers: [
